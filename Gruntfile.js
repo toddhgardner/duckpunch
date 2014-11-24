b07bbffb51889d98b9312f40405ca53f7823d13e
Todd H. Gardner <todd@wocketware.com>
2014-11-24 16:23:41 -0600

    ajaxing form as a base for the info-jacking

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,41 @@
 module.exports = function(grunt) {
 
-  grunt.registerTask("default", ["connect:server"]);
+  grunt.registerTask("default", [
+    "connect:cdn",
+    "connect:server"]);
 
   grunt.loadNpmTasks("grunt-contrib-connect");
 
   grunt.initConfig({
     connect: {
+
+      // The primary webserver
       server: {
         options: {
           port: 3000,
           base: "src",
-          keepalive: true
+          keepalive: true,
+          middleware: function (connect, options, middleware) {
+            middleware.unshift(["/api/emails", function (req, res, next) {
+              var body = "";
+              req.on("data", function (data) {
+                body += data.toString();
+              });
+              req.on("end", function () {
+                console.log(body);
+                res.end("{ id: '1' }");
+              });
+            }]);
+            return middleware;
+          }
+        }
+      },
+
+      // Represents a third-party CDN that we might load scripts from
+      cdn: {
+        options: {
+          port: 3001,
+          base: "src"
         }
       }
     }
---
+++ src/infojack/main.js
@@ -0,0 +1,37 @@
+(function (window, document) {
+  "use strict";
+  document.addEventListener("DOMContentLoaded", function onLoad (event) {
+
+    // References to the DOM Elements.
+    var form = document.getElementById("form-email");
+    var email = document.getElementById("input-email");
+    var message = document.getElementById("form-success");
+
+    // When the form is submitted
+    form.addEventListener("submit", function onSubmit (event) {
+
+      // Setup a new AJAX request to our API
+      var request = new XMLHttpRequest();
+      request.open("post", "/api/emails");
+
+      // When it's done, show the thank-you message
+      request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+          form.style.display = "none";
+          message.style.display = "block";
+        }
+      }
+      
+      // Send the request with the serialized email
+      request.send(JSON.stringify({
+        email: email.value
+      }));
+
+      // Prevent the standard form post action from happening
+      event.preventDefault();
+      return false;
+    });
+
+  });
+
+})(window, document);
